f39c4ca62d244a1f769e2d2c0421a743d82ed809
mjgaetano <102186693+mjgaetano@users.noreply.github.com>
2023-03-06 12:19:24 -0500

    Added code for basic window

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import pygame
 
-#Initialize PyGame
+# Initialize PyGame
 pygame.init()
+
+# Create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# Title and Icon
+pygame.display.set_caption("Monopoly | Ski Resort Edition")
+icon = pygame.image.load("ski-resort.png")
+pygame.display.set_icon(icon)
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((135, 206, 235))
+    pygame.display.update()
