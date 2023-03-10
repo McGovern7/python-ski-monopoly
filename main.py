e56ceed72df2edc25e3c44e196c408c09bf1e4cc
mjgaetano <102186693+mjgaetano@users.noreply.github.com>
2023-03-09 19:15:22 -0500

    Modified formatting

--- main.py
+++ main.py
@@ -1,61 +1,26 @@
 import sys
-import pygame
+import pygame as pg
 from Bank_Account import Bank_Account
 
-#Initialize PyGame
-pygame.init()
 
-#make screen
-screen = pygame.display.set_mode((1200,800))
-
-#constants
-#TODO - which font should we use?
-#print(pygame.font.get_fonts())
-font = pygame.font.SysFont("times", 30)
-small_font_1 = pygame.font.SysFont("times", 16)
-small_font_2 = pygame.font.SysFont("times", 15)
-small_font_3 = pygame.font.SysFont("times", 12)
-small_font_4 = pygame.font.SysFont("times", 10)
-green = (0, 100, 0)
-white = (255,255,255)
-black = (0,0,0)
-blue = (30, 144, 225)
-
-
-
-#TODO - was just messing around with player icons (definitely feel free to change)
-#Players
-#P1
-player1_icon = pygame.image.load("skiing.png")
-player1_x = 300
-player1_y = 600
-# #P2
-# player2_icon = pygame.image.load("deposit.png")
-# player2_x= 500
-# player2_y= 600
-# #P3
-# player3_icon = pygame.image.load("deposit.png")
-# player3_x = 300
-# player3_y = 300
-# #P4
-# player4_icon = pygame.image.load("deposit.png")
-# player4_x = 300
-# player4_y = 300
 def player1():
     screen.blit(player1_icon, (player1_x, player1_y))
 
-#house graphic
-def create_house(x,y):
+
+# house graphic
+def create_house(x, y):
     house = pygame.image.load("home.png")
     screen.blit(house, (x, y))
 
-#hotel graphic
-def create_hotel(x,y):
+
+# hotel graphic
+def create_hotel(x, y):
     hotel = pygame.image.load("hotel.png.png")
     screen.blit(hotel, (x, y))
 
+
 def create_card(x, y, region_color):
-    #text on every property card
+    # text on every property card
     deed_text = small_font_4.render("TITLE DEED", True, black)
     property_text = small_font_1.render("Property Name", True, black)
     rent_text = small_font_2.render("Rent: $", True, black)
@@ -66,9 +31,9 @@ def create_card(x, y, region_color):
     hotel_text = small_font_3.render("With Hotel", True, black)
     cost_text_1 = small_font_3.render("Houses cost $", True, black)
     cost_text_2 = small_font_3.render("Hotel costs $", True, black)
-    #draw
+    # draw
     pygame.draw.rect(screen, white, (x, y, 150, 200))
-    pygame.draw.rect(screen, region_color, (x+5, y+5, 140, 50))
+    pygame.draw.rect(screen, region_color, (x + 5, y + 5, 140, 50))
     screen.blit(deed_text, (90, 160))
     screen.blit(property_text, (70, 180))
     screen.blit(rent_text, (90, 215))
@@ -80,8 +45,16 @@ def create_card(x, y, region_color):
     screen.blit(cost_text_1, (60, 320))
     screen.blit(cost_text_2, (60, 330))
 
-#SCREENS
-#start screen
+
+def roll_dice(pg):
+    '''
+
+    :return:
+    '''
+
+
+# SCREENS
+# start screen
 def start_screen():
     # game title and icon
     pygame.display.set_caption("Monopoly | Ski Resort Edition")
@@ -89,12 +62,15 @@ def start_screen():
     pygame.display.set_icon(icon)
     while True:
         return True
-#board screen
+
+
+# board screen
 def board_screen():
     while True:
         return True
 
-#card screen
+
+# card screen
 def card_screen():
     pygame.display.set_caption("Your cards")
     text1 = font.render("Available money: ", True, white)
@@ -103,7 +79,7 @@ def card_screen():
         screen.fill(green)
         screen.blit(text1, (50, 50))
         screen.blit(text2, (50, 100))
-        #create card
+        # create card
         create_card(50, 150, blue)
         create_house(100, 500)
 
@@ -116,14 +92,62 @@ def card_screen():
         return True
 
 
-# Game loop
-while True:
-    #start_screen()
-    card_screen()
+def main():
+    # Initialize PyGame
+    pygame.init()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    # make screen
+    screen = pygame.display.set_mode((1200, 800))
 
-    pygame.display.update()
+    # constants
+    # TODO - which font should we use?
+    # print(pygame.font.get_fonts())
+    FONT_NAME = "times"
+    font = pygame.font.SysFont(FONT_NAME, 30)
+    small_font_1 = pygame.font.SysFont(FONT_NAME, 16)
+    small_font_2 = pygame.font.SysFont(FONT_NAME, 15)
+    small_font_3 = pygame.font.SysFont(FONT_NAME, 12)
+    small_font_4 = pygame.font.SysFont(FONT_NAME, 10)
+    green = (0, 100, 0)
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    blue = (30, 144, 225)
+
+    # TODO - was just messing around with player icons (definitely feel free to change)
+    # Players
+    # P1
+    player1_icon = pygame.image.load("skiing.png")
+    player1_x = 300
+    player1_y = 600
+    # # P2
+    # player2_icon = pygame.image.load("deposit.png")
+    # player2_x= 500
+    # player2_y= 600
+    # # P3
+    # player3_icon = pygame.image.load("deposit.png")
+    # player3_x = 300
+    # player3_y = 300
+    # # P4
+    # player4_icon = pygame.image.load("deposit.png")
+    # player4_x = 300
+    # player4_y = 300
+
+    # Game loop
+    TEST_DICE = True
+    while True:
+        # start_screen()
+        card_screen()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        if TEST_DICE:
+            roll_dice(pygame)
+
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
