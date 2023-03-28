9a9befc6792468dd6e7168bb0ab4d85df547ba9e
mjgaetano <102186693+mjgaetano@users.noreply.github.com>
2023-03-28 13:32:33 -0400

    Modified multiplayer game

--- client_tester.py
+++ client_tester.py
@@ -1,5 +1,6 @@
 import pygame
-from Network import Network
+from network import Network
+from player import Player
 
 pygame.init()
 
@@ -7,51 +8,6 @@ width = 500
 height = 500
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
-
-client_number = 0
-
-
-class Player:
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.rect = (x, y, width, height)
-        self.vel = 3
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, self.rect)
-
-    def move(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_LEFT]:
-            self.x -= self.vel
-
-        if keys[pygame.K_RIGHT]:
-            self.x += self.vel
-
-        if keys[pygame.K_UP]:
-            self.y -= self.vel
-
-        if keys[pygame.K_DOWN]:
-            self.y += self.vel
-
-        self.update()
-
-    def update(self):
-        self.rect = (self.x, self.y, self.width, self.height)
-
-
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
 
 
 def redraw_window(win, player1, player2):
@@ -64,18 +20,13 @@ def redraw_window(win, player1, player2):
 def main():
     run = True
     n = Network()
-    start_pos = read_pos(n.get_pos())
-    p = Player(start_pos[0], start_pos[1], 100, 100, (0, 255, 0))
-    p2 = Player(0, 0, 100, 100, (255, 0, 0))
+    p = n.get_p()
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
-
-        p2_pos = read_pos(n.send(make_pos((p.x, p.y))))
-        p2.x = p2_pos[0]
-        p2.y = p2_pos[1]
-        p2.update()
+        p2 = n.send(p)
+        p2.set_color((255, 0, 0))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 import socket
 from _thread import *
 import sys
+from player import Player
+import pickle
 
 # server = "162.247.88.151" # Lofts
-server = "10.245.202.223" # UVM
+# server = "10.245.202.223" # UVM
+server = ""
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,40 +19,29 @@ except socket.error as e:
 s.listen(2)
 print("Server started. Waiting for a connection...")
 
-
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-
-pos = [(0, 0), (100, 100)]
+players = [Player(0, 0, 50, 50, (0, 255, 0)), Player(100, 100, 50, 50, (0, 255, 0))]
 
 
 def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
-    reply = ""
+    conn.send(pickle.dumps(players[player]))
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
 
                 # print("Received: ", data)
                 # print("Sending: ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
         except:
             break
 
