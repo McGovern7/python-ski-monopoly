4f74a662756cc2ea645dd583681941a69c836e7d
raatherly <rachel.atherly@uvm.edu>
2023-03-28 18:22:08 -0400

    top row center points identified

--- main.py
+++ main.py
@@ -300,15 +300,20 @@ def board_screen(screen):
 
     # tile lines
     y = centerY
-    y_icon = centerY/18
+    #y_icon = centerY/18
     for i in range(10):
         pygame.draw.rect(screen, black, (0, y, centerY, 1))
-        #trying to find spot
-        pygame.draw.rect(screen, blue, (55, 55, 4, 4))
         pygame.draw.rect(screen, black, (centerX + centerDimension, y, centerY, 1))
         y += centerDimension / 9  # Spaces all the squares evenly
 
     x = centerX
+    coordLocation = centerX
+    # trying to find spot
+    pygame.draw.rect(screen, red, (55, 55, 4, 4))
+    for i in range(9):
+        pygame.draw.rect(screen, red, (coordLocation + (575/9)/2, 55, 4, 4))
+        coordLocation += (centerDimension/9)
+
     for i in range(10):
         pygame.draw.rect(screen, black, (x, 0, 1, centerX))
         pygame.draw.rect(screen, black, (x, centerY + centerDimension, 1, centerX))
